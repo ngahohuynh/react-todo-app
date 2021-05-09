853083306474a98f32443e7496baeaeac61fc6ff
Ho Huynh Nga <hnga3511@gmail.com>
2021-05-09 17:01:10 +0700

    WIP

--- src/App.js
+++ src/App.js
@@ -4,24 +4,24 @@ import TodosList from './components/TodosList';
 
 const TODOS_DATA = [
   {
-      id: 't1',
-      title: 'Code',
-      completed: true
+    id: 't1',
+    title: 'Code',
+    completed: true
   },
   {
-      id: 't2',
-      title: 'Exercise',
-      completed: false
+    id: 't2',
+    title: 'Exercise',
+    completed: false
   },
   {
-      id: 't3',
-      title: 'Meditate',
-      completed: false
+    id: 't3',
+    title: 'Meditate',
+    completed: false
   },
   {
-      id: 't4',
-      title: 'Read for 1 hour',
-      completed: true
+    id: 't4',
+    title: 'Read for 1 hour',
+    completed: true
   }
 ];
 
@@ -29,10 +29,15 @@ function App() {
   const [todosList, setTodosList] = useState(TODOS_DATA);
 
   const addTodo = (title) => {
+    if (!title) { return; }
     setTodosList(prevList => {
       return [...prevList, { id: Math.random().toString(36).substr(2, 9), title: title, completed: false }];
     });
-  }
+  };
+
+  const removeTodo = (id) => {
+    setTodosList(prevState => prevState.filter(item => item.id !== id));
+  };
 
   return (
     <Fragment>
@@ -41,7 +46,10 @@ function App() {
       </header>
       <main>
         <AddTodo onAddTodo={addTodo} />
-        <TodosList todosList={todosList} />
+        <TodosList 
+          items={todosList} 
+          onRemoveItem={removeTodo} 
+        />
       </main>
     </Fragment>
   );
--- src/components/TodosList.js
+++ src/components/TodosList.js
@@ -1,29 +1,51 @@
-import { Fragment } from "react";
+import { Fragment, useState } from "react";
 import TodoItem from "./TodoItem";
 
 const TodosList = (props) => {
-    const toggleTodoCompleted = (id) => {
+    const [statusToFilter, setStatusToFilter] = useState();
+
+    const toggleCompleted = (id) => {
     }
+
+    const getAll = () => {
+        setStatusToFilter();
+    };
+
+    const getActiveTodos = () => {
+        setStatusToFilter(false);
+    };
+
+    const getCompletedTodos = () => {
+        setStatusToFilter(true);
+    };
+
+    const removeCompleted = () => {
+    };
+
+    const shownList = typeof statusToFilter === "boolean" ? 
+        props.items.filter(item => item.completed === statusToFilter) : 
+        props.items;
+    const itemsLeft = props.items.filter(item => item.completed === false).length;
 
     return (
         <Fragment>
             <ul className="todos">
-                {props.todosList.map(item => (
+                {shownList.map(item => (
                     <TodoItem 
                         key={item.id}
                         id={item.id}
                         title={item.title}
                         completed={item.completed}
-                        toggleStatus={toggleTodoCompleted}
+                        toggleStatus={toggleCompleted}
                     />
                 ))}
             </ul>
             <div className="card stat">
-                <p className="corner"><span id="items-left">0</span> items left</p>
+                <p className="corner"><span id="items-left">{itemsLeft}</span> items left</p>
                 <div className="filter">
-                    <button id="all" className="on">All</button>
-                    <button id="active">Active</button>
-                    <button id="completed">Completed</button>
+                    <button id="all" className={`${typeof statusToFilter !== "boolean" ? 'on' : ''}`} onClick={getAll}>All</button>
+                    <button id="active" className={`${statusToFilter === false ? 'on' : ''}`} onClick={getActiveTodos}>Active</button>
+                    <button id="completed" className={`${statusToFilter === true ? 'on' : ''}`} onClick={getCompletedTodos}>Completed</button>
                 </div>
                 <div className="corner">
                     <button id="clear-completed">Remove Completed</button>
