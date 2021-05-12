a0165d2d60e23829b4de6b085ea46afac25a0348
Ho Huynh Nga <hnga3511@gmail.com>
2021-05-12 12:40:10 +0700

    Implement store

---
+++ src/store/index.js
@@ -0,0 +1,75 @@
+import { createStore } from 'redux';
+
+const initialState = {
+    todos: [],
+    statusToFilter: null
+};
+
+const todoReducer = (state = initialState, action) => {
+    switch (action.type) {
+        case 'ADD':
+            return {
+                todos: [...state.todos, {
+                    id: Math.random().toString(36).substr(2, 9),
+                    title: action.title,
+                    completed: false
+                }],
+                statusToFilter: state.statusToFilter
+            };
+
+        case 'EDIT':
+            let updatedList = [...state.todos];
+            const index = state.todos.findIndex(item => item.id === action.id);
+            updatedList[index] = {
+                ...updatedList[index],
+                title: action.newTitle
+            };
+            return {
+                todos: updatedList,
+                statusToFilter: state.statusToFilter
+            };
+
+        case 'REMOVE':
+            return {
+                todos: state.todos.filter(item => item.id !== action.id),
+                statusToFilter: state.statusToFilter
+            };
+
+        case 'TOGGLE_STATUS':
+            let newList = [...state.todos];
+            const foundIndex = state.todos.findIndex(item => item.id === action.id);
+            newList[foundIndex] = {
+                ...newList[foundIndex],
+                completed: !newList[foundIndex].completed
+            };
+            return {
+                todos: newList,
+                statusToFilter: state.statusToFilter
+            };
+
+        case 'REMOVE_COMPLETED':
+            return {
+                todos: state.todos.filter(item => item.completed === false),
+                statusToFilter: state.statusToFilter
+            };
+
+        case 'FILTER_NONE':
+            return {
+                todos: state.todos,
+                statusToFilter: null
+            };
+
+        case 'FILTER_STATUS':
+            return {
+                todos: state.todos.filter(item => item.completed === action.status),
+                statusToFilter: action.status
+            };
+
+        default:
+            return state;
+    }
+};
+
+const store = createStore(todoReducer);
+
+export default store;
